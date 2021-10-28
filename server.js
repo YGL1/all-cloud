0aa5177da7b085a59250a9e79ab412ca7156a4df
YGL1 <46554130+YGL1@users.noreply.github.com>
2021-10-28 10:04:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ function requireHTTPS(req, res, next) {
 const express = require('express');
 const app = express();
 app.use(requireHTTPS);
-//app.use(express.static('./dist/exercise-all-cloud'));
-app.use(express.static(__dirname + '/dist/exercise-all-cloud'));
+app.use(express.static('./dist/exercise-all-cloud'));
+//app.use(express.static(__dirname + '/dist/exercise-all-cloud'));
 // app.use(express.static(path.join(__dirname, './build')));
 
 app.get('/*', function(req, res) {
-    console.log(__dirname);
-    const indexFile = path.resolve(__dirname + '/dist/exerciseAllCloud/index.html');
-    res.sendFile(indexFile);
-    //res.sendFile('index.html', {root: 'dist/exerciseAllCloud/'});
+    //console.log(__dirname);
+    //const indexFile = path.resolve(__dirname + '/dist/exerciseAllCloud/index.html');
+    //res.sendFile(indexFile);
+    res.sendFile('index.html', {root: './dist/exerciseAllCloud/'});
     // res.sendFile(path.join(__dirname, '../dist/exercise-all-cloud/index.html'));
     //console.log("Hello222222222222222222222");
     });
